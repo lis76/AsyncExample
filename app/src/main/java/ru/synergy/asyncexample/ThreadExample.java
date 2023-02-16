dceaaf998c99af7a7392fb1793dd7a2dc50ca5f8
lis76 <rsavoskin@yandex.ru>
2023-02-16 14:48:36 +0300

    Finished Sparrow Counter Example

--- app/src/main/java/ru/synergy/asyncexample/ThreadExample.java
+++ app/src/main/java/ru/synergy/asyncexample/ThreadExample.java
@@ -1,14 +1,56 @@
 package ru.synergy.asyncexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class ThreadExample extends AppCompatActivity {
+
+    int mCounter;
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            TextView mInfoTextView = (TextView) findViewById(R.id.textViewinfo);
+            mInfoTextView.setText("Сегодня ворон было" + mCounter + "Штук");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread_example);
+    }
+    public void onClick(View v){
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                long endTime = System.currentTimeMillis() + 20 * 1000;
+                while (System.currentTimeMillis()< endTime){
+                    synchronized (this){
+                        try {
+                            wait(endTime-System.currentTimeMillis());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Log.i("SPARROWS", ("Сегодня ворон было " + mCounter++ + " Штук"));
+                    handler.sendEmptyMessage(0);
+                }
+
+                // Так делать нельзя
+
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 }

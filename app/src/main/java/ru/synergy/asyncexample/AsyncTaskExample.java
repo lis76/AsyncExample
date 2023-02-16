d66164253f94c0aadb8fb7acae68d2f7e2d4bc8a
lis76 <rsavoskin@yandex.ru>
2023-02-16 10:47:50 +0300

    Example AsyncTask class structure

--- app/src/main/java/ru/synergy/asyncexample/AsyncTaskExample.java
+++ app/src/main/java/ru/synergy/asyncexample/AsyncTaskExample.java
@@ -2,6 +2,7 @@ package ru.synergy.asyncexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class AsyncTaskExample extends AppCompatActivity {
@@ -10,5 +11,41 @@ public class AsyncTaskExample extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async_task_example);
+
+        MyAsyncTask asynctask = new MyAsyncTask();
+        asynctask.execute("Hello World!");
     }
+}
+
+class MyAsyncTask extends AsyncTask<String, Integer, Integer> {
+
+
+    @Override
+    protected Integer doInBackground(String... strings) {
+        int myProgress = 0;
+        publishProgress(myProgress);
+        int result = myProgress++;
+        return result;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        super.onPostExecute(integer);
+    }
+
 }
